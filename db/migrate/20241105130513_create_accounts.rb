a829601ac071da4001f6800b3343e5461fb314cf
AtanaelVmax22 <xplayerdownloadx@gmail.com>
2024-11-05 10:31:03 -0300

    Polimorficas-Associacao

--- db/migrate/20241105130513_create_accounts.rb
+++ db/migrate/20241105130513_create_accounts.rb
@@ -3,8 +3,10 @@ class CreateAccounts < ActiveRecord::Migration[7.2]
     create_table :accounts do |t|
       t.string :account_number
       t.references :supplier, null: false, foreign_key: true
-
+      t.bigint  :supplier_id
+      t.string  :account_number
       t.timestamps
     end
+    add_index :accounts, :supplier_id
   end
 end
